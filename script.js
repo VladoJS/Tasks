236f76d17d31ba9f75f0ab3e847ad487f103db50
Vlad <example@gmail.com>
2022-03-16 13:50:17 +0300

    short word

--- script.js
+++ script.js
@@ -8,10 +8,8 @@ function isPalindrome(str) {
     let res = str === resStr;
     return res;
 }
-
 console.log(isPalindrome('noon'));
 console.log(isPalindrome('moon'));
-
 
 // 2:
 const isPalindromeTwo = str =>
@@ -19,3 +17,24 @@ const isPalindromeTwo = str =>
 
 console.log(isPalindromeTwo('moon'));
 console.log(isPalindromeTwo('defined'));
+
+
+// find short 
+// 1:
+function findShort(str) {
+    let wordsArr = str.split(' ');
+    let sortWordsArr = wordsArr.sort(function(a, b) {
+        return a.length - b.length;
+    });
+    return sortWordsArr[0];
+}
+console.log(findShort('Сould you tell me about yourself?'));
+
+
+// 2:
+const findShortWord = str =>
+  str
+    .split(' ')
+    .sort((a, b) => a.length - b.length)[0];
+
+console.log(findShortWord('How are you doing'));
