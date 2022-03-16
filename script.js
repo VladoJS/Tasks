1d8cc2135e14f7fca88b1cb8a13a6d25e2c4d09a
Vlad <example@gmail.com>
2022-03-16 13:30:22 +0300

    palindrome

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 // create a palindrome check function
 // example: noon,  deed, deified
+// 1:
+function isPalindrome(str) {
+    let arr = str.split('');
+    let revArr = arr.reverse();
+    let resStr = revArr.join('');
+    let res = str === resStr;
+    return res;
+}
 
+console.log(isPalindrome('noon'));
+console.log(isPalindrome('moon'));
+
+
+// 2:
+const isPalindromeTwo = str =>
+    str === str.split('').reverse().join('');
+
+console.log(isPalindromeTwo('moon'));
+console.log(isPalindromeTwo('defined'));
